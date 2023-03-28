26c2e4f802f760af9f9a5982e6d400942023f424
feddorovich <feddorovich@outlook.com>
2023-03-28 21:38:51 +0300

    completing Projects

--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -7,10 +7,13 @@ type PropsType = {
     description: string
 }
 
-export const Project: FC = () => {
+export const Project: FC<PropsType> = (props) => {
     return (
         <div className={style.project}>
-            Project
+            <div className={style.photo}>IMAGE</div>
+            {/*<button>BUTTON</button>*/}
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
         </div>
     );
 };
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,6 +3,21 @@ import style from './Skills.module.css'
 import {Skill} from "./skill/Skill";
 import {Container} from "../container/Container";
 
+type SkillDataType = {
+    id: number
+    title: string
+    description: string
+}
+
+const skillData: SkillDataType[] = [
+    {id: 1,title: 'JS', description: 'The quick brown fox jumps over the lazy dog, but the lazy dog just yawns and goes back to sleep.'},
+    {id: 2,title: 'CSS', description: 'Life is like a camera. Focus on the good times, develop from the negatives, and if things don\'t work out, take another shot.'},
+    {id: 3,title: 'React', description: 'In the middle of every difficulty lies opportunity.'},
+    {id: 4,title: 'JS', description: 'The quick brown fox jumps over the lazy dog, but the lazy dog just yawns and goes back to sleep.'},
+    {id: 5,title: 'CSS', description: 'Life is like a camera. Focus on the good times, develop from the negatives, and if things don\'t work out, take another shot.'},
+    {id: 6,title: 'React', description: 'In the middle of every difficulty lies opportunity.'}
+]
+
 export const Skills = () => {
     return (
         <section className={style.skillsBlock}>
@@ -10,16 +25,11 @@ export const Skills = () => {
                 <div className={style.wrapper}>
                     <h2 className={style.title}>My Skills</h2>
                     <div className={style.skills}>
-                        <Skill title={'JS'}
-                               description={'The quick brown fox jumps over the lazy dog, but the lazy dog just yawns and goes back to sleep.'}/>
-                        <Skill title={'CSS'}
-                               description={'Life is like a camera. Focus on the good times, develop from the negatives, and if things don\'t work out, take another shot.'}/>
-                        <Skill title={'React'} description={'In the middle of every difficulty lies opportunity.'}/>
-                        <Skill title={'JS'}
-                               description={'The quick brown fox jumps over the lazy dog, but the lazy dog just yawns and goes back to sleep.'}/>
-                        <Skill title={'CSS'}
-                               description={'Life is like a camera. Focus on the good times, develop from the negatives, and if things don\'t work out, take another shot.'}/>
-                        <Skill title={'React'} description={'In the middle of every difficulty lies opportunity.'}/>
+                        {skillData.map(s => {
+                            return (
+                                <Skill key={s.id} title={s.title} description={s.description}/>
+                            )
+                        })}
                     </div>
                 </div>
             </Container>
