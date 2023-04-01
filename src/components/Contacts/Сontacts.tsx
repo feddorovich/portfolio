6c1f5698e3b756df7b91050d34077fbaba4fa96a
feddorovich <feddorovich@outlook.com>
2023-04-01 18:55:52 +0300

    Contacts component with CSS

--- src/components/Contacts/Сontacts.tsx
+++ src/components/Contacts/Сontacts.tsx
@@ -1,13 +1,26 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import style from './Contacts.module.css'
 import {Container} from "../Container/Container";
 
 export const Сontacts: FC = () => {
+
     return (
         <section className={style.contactsBlock}>
             <Container>
                 <div className={style.wrapper}>
-                    Contacts
+                    <h2 className={style.title}>Сontacts</h2>
+                    <form>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" id="name" name="name"/>
+
+                        <label htmlFor="email">E-mail:</label>
+                        <input type="email" id="email" name="email"/>
+
+                        <label htmlFor="message">Message:</label>
+                        <textarea id="message" name="message" rows={5}></textarea>
+
+                        <button type="submit">Submit</button>
+                    </form>
                 </div>
             </Container>
         </section>
