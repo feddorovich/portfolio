9f9c085ba30602370ecf11a7a9da0140a9c79e51
feddorovich <feddorovich@outlook.com>
2023-04-03 16:22:27 +0300

    project fix photo

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -10,8 +10,7 @@ type PropsType = {
 export const Project: FC<PropsType> = (props) => {
     return (
         <div className={style.project}>
-            <div className={style.photo}>
-                Hover me
+            <div className={style.photo} style={{backgroundImage: `url(${props.img})`}}>
                 <button className={style.btn}>View</button>
             </div>
             <h3>{props.title}</h3>
