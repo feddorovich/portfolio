056e81a5c0f3a6f883fd2f6af708c553b3c854dc
feddorovich <feddorovich@outlook.com>
2023-04-20 21:50:42 +0300

    Header.module.css customize

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,15 +2,27 @@ import React, {FC} from 'react';
 import style from './Header.module.css'
 import {Nav} from "../Nav/Nav";
 import {Container} from "../Container/Container";
+import {NavLink} from "react-router-dom";
 
 export const Header: FC = () => {
+
+    const handleNavLinkClick = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+
     return (
         <header className={style.header}>
             <Container>
                 <div className={style.wrapper}>
+                    <div className={style.logo}>
+                        <NavLink to={'/'}
+                                 onClick={handleNavLinkClick}
+                        >Logo</NavLink>
+                    </div>
                     <Nav/>
+                    <div>Socials</div>
                 </div>
             </Container>
         </header>
-    );
-};
+    )
+}
