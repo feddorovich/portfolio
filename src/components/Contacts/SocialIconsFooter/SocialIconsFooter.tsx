410960e9fc671091ae17417b6a4b1cc1f32aafca
feddorovich <feddorovich@outlook.com>
2023-04-30 16:00:44 +0300

    social icons scr

--- src/components/Contacts/SocialIconsFooter/SocialIconsFooter.tsx
+++ src/components/Contacts/SocialIconsFooter/SocialIconsFooter.tsx
@@ -6,16 +6,16 @@ import { SiCodewars } from 'react-icons/si';
 export const SocialIconsFooter = () => {
     return (
         <div className={style.socialIcons}>
-            <a href="https://github.com" target={'_blank'} className={style.iconLink}>
+            <a href="https://github.com/feddorovich" target={'_blank'} className={style.iconLink}>
                 <BsGithub className={style.git} />
             </a>
-            <a href="https://telegram.org" target={'_blank'} className={style.iconLink}>
+            <a href="https://t.me/feddorovich" target={'_blank'} className={style.iconLink}>
                 <BsTelegram className={style.tg} />
             </a>
-            <a href="https://www.linkedin.com" target={'_blank'} className={style.iconLink}>
+            <a href="https://www.linkedin.com/in/feddorovich/" target={'_blank'} className={style.iconLink}>
                 <BsLinkedin className={style.link} />
             </a>
-            <a href="https://www.codewars.com" target={'_blank'} className={style.iconLink}>
+            <a href="https://www.codewars.com/users/feddorovich" target={'_blank'} className={style.iconLink}>
                 <SiCodewars className={style.cd} />
             </a>
         </div>
