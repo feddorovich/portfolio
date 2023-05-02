1134d89bf5d79f3b285dabea8c6b4d6ac9195fb4
feddorovich <feddorovich@outlook.com>
2023-05-02 20:46:46 +0300

    fix Skills.tsx

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -5,75 +5,31 @@ import {Container} from "../Container/Container";
 import {Animation} from "../Animation/Animation";
 import {
     SiReact, SiRedux, SiJavascript, SiTypescript, SiAxios, SiJest, SiSass,
-    SiStyledcomponents, SiStorybook, SiMui, SiTestcafe
+    SiStyledcomponents, SiStorybook, SiMui, SiTestcafe, SiVuedotjs
 } from "react-icons/si";
 import {IconType} from "react-icons/lib";
 
 type SkillDataType = {
-    id: number
     Icon: IconType
     title: string
 }
 
 const skillData: SkillDataType[] = [
-    {
-        id: 1,
-        Icon: SiReact,
-        title: 'React'
-    },
-    {
-        id: 2,
-        Icon: SiRedux,
-        title: 'Redux'
-    },
-    {
-        id: 3,
-        Icon: SiJavascript,
-        title: 'JavaScript'
-    },
-    {
-        id: 4,
-        Icon: SiTypescript,
-        title: 'TypeScript'
-    },
-    {
-        id: 5,
-        Icon: SiAxios,
-        title: 'Axios'
-    },
-    {
-        id: 6,
-        Icon: SiJest,
-        title: 'Jest'
-    },
-    {
-        id: 7,
-        Icon: SiSass,
-        title: 'SASS'
-    },
-    {
-        id: 8,
-        Icon: SiStyledcomponents,
-        title: 'Styled-Components'
-    },
-    {
-        id: 9,
-        Icon: SiStorybook,
-        title: 'StoryBook'
-    },
-    {
-        id: 10,
-        Icon: SiMui,
-        title: 'MUI'
-    },
-    {
-        id: 11,
-        Icon: SiTestcafe,
-        title: 'Unit-tests'
-    },
+    {Icon: SiReact, title: 'React'},
+    {Icon: SiRedux, title: 'Redux'},
+    {Icon: SiJavascript, title: 'JavaScript'},
+    {Icon: SiTypescript, title: 'TypeScript'},
+    {Icon: SiVuedotjs, title: 'Vue'},
+    {Icon: SiAxios, title: 'Axios'},
+    {Icon: SiJest, title: 'Jest'},
+    {Icon: SiSass, title: 'SASS'},
+    {Icon: SiStyledcomponents, title: 'Styled-Components'},
+    {Icon: SiStorybook, title: 'StoryBook'},
+    {Icon: SiMui, title: 'MUI'},
+    {Icon: SiTestcafe, title: 'Unit-tests'},
 ]
 
-export const Skills:FC = () => {
+export const Skills: FC = () => {
     return (
         <section className={style.skillsBlock} id={'skills'}>
             <Container>
@@ -85,9 +41,9 @@ export const Skills:FC = () => {
                         </div>
                     </Animation>
                     <div className={style.skills}>
-                        {skillData.map(s => {
+                        {skillData.map((s, i) => {
                             return (
-                                <Skill key={s.id} {...s} />
+                                <Skill key={i} {...s} />
                             )
                         })}
                     </div>
