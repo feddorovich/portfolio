5d6315bae38aa2b4e72a7e6e6836431d6240810c
feddorovich <feddorovich@outlook.com>
2023-05-02 16:39:17 +0300

    refactor icons

--- src/components/Header/SocialIcons/SocialIcons.tsx
+++ src/components/Header/SocialIcons/SocialIcons.tsx
@@ -6,14 +6,14 @@ import { SiCodewars } from 'react-icons/si';
 export const SocialIcons = () => {
     return (
         <div className={style.socialIcons}>
+            <a href="https://www.linkedin.com/in/feddorovich/" target={'_blank'} className={style.iconLink}>
+                <BsLinkedin className={style.link} />
+            </a>
             <a href="https://github.com/feddorovich" target={'_blank'} className={style.iconLink}>
                 <BsGithub className={style.git} />
             </a>
             <a href="https://t.me/feddorovich" target={'_blank'} className={style.iconLink}>
                 <BsTelegram className={style.tg} />
-            </a>
-            <a href="https://www.linkedin.com/in/feddorovich/" target={'_blank'} className={style.iconLink}>
-                <BsLinkedin className={style.link} />
             </a>
             <a href="https://www.codewars.com/users/feddorovich" target={'_blank'} className={style.iconLink}>
                 <SiCodewars className={style.cd} />
