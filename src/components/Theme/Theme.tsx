725878adf58e2e69fedccaa32afffe4107434c9d
feddorovich <feddorovich@outlook.com>
2023-05-15 17:57:01 +0300

    dark mode desktop

--- src/components/Theme/Theme.tsx
+++ src/components/Theme/Theme.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 const updateTheme = (theme: string) => {
     const root = document.querySelector(':root') as HTMLElement;
 
-    const components = ['bgPrimary', 'text'];
+    const components = ['bgPrimary', 'text', 'bgSecondary', 'textSecondary', 'boxShadow'];
     components.forEach(c => {
         root.style.setProperty(
             `--${c}Default`,
