2b2b62bfd43e6c4b99abbc9e00d5884b44a825be
feddorovich <feddorovich@outlook.com>
2023-07-03 11:11:37 +0300

    refactor ScrollToTop.tsx

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,36 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { BsArrowUpCircle, BsFillArrowUpSquareFill } from 'react-icons/bs'
+import React, { useEffect, useState } from 'react'
+import { BsArrowUpCircle } from 'react-icons/bs'
 import s from './ScrollToTop.module.scss'
 
 export const ScrollToTop: React.FC = () => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false)
 
   const handleScroll = () => {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    setIsVisible(scrollTop > 100);
-  };
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    setIsVisible(scrollTop > 100)
+  }
 
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
-    });
-  };
+    })
+  }
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
-    <div
-      className={`${s.container} ${isVisible ? s.visible : ''}`}
-      onClick={scrollToTop}
-    >
-      <BsArrowUpCircle className={s.icon}/>
-      {/*Наверх*/}
+    <div className={`${s.container} ${isVisible ? s.visible : ''}`} onClick={scrollToTop}>
+      <BsArrowUpCircle className={s.icon} />
     </div>
-  );
-};
+  )
+}
