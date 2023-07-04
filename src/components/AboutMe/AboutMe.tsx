2a044661e88c0e46994be30627d76e6445f364c3
feddorovich <feddorovich@outlook.com>
2023-07-04 15:47:17 +0300

    add aboutMe component

---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import style from './AboutMe.module.scss'
+import { Container } from '../Container/Container'
+import { Animation } from '../Animation/Animation'
+
+export const AboutMe = () => {
+  return (
+    <section className={style.aboutMeBlock} id={'skills'}>
+      <Container>
+        <div className={style.wrapper}>
+          <Animation animation={'animate__fadeInUp'} threshold={0.4}>
+            <div className={style.title}>
+              <h3>About Me</h3>
+              <h2>Know Me More</h2>
+            </div>
+          </Animation>
+          <div className={style.infoBlock}>
+            <div>
+              <span className={style.name}>Hi, I'm</span>
+              <p>
+                I'm a designer & developer with a passion for web design. I enjoy developing simple, clean and slick
+                websites that provide real value to the end user. Thousands of clients have procured exceptional results
+                while working with me. Delivering work within time and budget which meets client’s requirements is our
+                moto.
+              </p>
+            </div>
+            <div>
+              <div>2</div>
+              <div>Years of Experience</div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  )
+}
